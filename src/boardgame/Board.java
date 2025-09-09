7ff3c39d59000076aca86497daedb2a26a672c78
Haylon Mosinho <haylonmosinho.dev@gmail.com>
2025-09-09 09:57:36 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -52,6 +52,19 @@ public class Board {
 		piece.position = position;
 	}
 	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		if (piece(position) == null){
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
 	
 	
 	public boolean positionExists(int row, int column) {
@@ -69,6 +82,8 @@ public class Board {
 		}
 		return piece(position) != null;
 	}
+	
+	
 	
 	
 }
